73d6d291c1577e1db755ab75bf788f4464019f54
Asaniyan Ayomide <heritageayomide2017@gmail.com>
2022-03-21 05:59:41 +0100

    added a loader and improved the searchForCountry, showSelectedCountry and showBorderCountries functionality

--- detail.js
+++ detail.js
@@ -1,15 +1,38 @@
 let countries; // will contain fetched data
-const Country = localStorage.getItem('Country');
+const Country = JSON.parse(localStorage.getItem('Country'));
 
 fetch('https://restcountries.com/v3.1/all')
-.then(res => res.json())
-.then(data => showSelectedCountry(data))
+.then(res => {
+    document.querySelector(".loader").style.visibility = "visible";
+    return res.json();
+})
+.then(data => {
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+    showSelectedCountry(data);
+})
 .catch(err => console.log(`Error: ${err}`));
+
 
 function showSelectedCountry(countryData) {
     countries = countryData;
-    const selectedCountryData = countries.find(country => country.name.common.toUpperCase() === Country.toUpperCase());
+    const selectedCountryData = countries.find(country => country.name.common.toUpperCase() === Country.toUpperCase() || country.cca3.toUpperCase() === Country.toUpperCase());
+
+
+    let contentDetails = document.querySelector('.content-details');
+
     let countryInfo = "";
+    let values = "";
+
+
+    if (selectedCountryData.languages !== undefined) {
+        let languages = Object.values(selectedCountryData.languages);
+        for (let i = 0; i < languages.length; i++) {
+            values +=  languages[i] + "\n";
+        }
+    } else {
+        values = "Not Available";
+    }
 
     countryInfo += `
     <div class="container-fluid mt-4">
@@ -21,42 +44,60 @@ function showSelectedCountry(countryData) {
                     <h2 class="country"> ${selectedCountryData.name.common} </h2>
 
                     <div class="col-12 features">
-                        <p class="col-md-6 col-sm-12 ">Native Name : <span class="nativeName" style="font-weight: 200;"> Uru</span></p>
+                        <p class="col-md-6 col-sm-12 ">Native Name : <span class="nativeName" style="font-weight: 200;">  ${selectedCountryData.name.common}</span></p>
 
-                        <p class="col-md-6 col-sm-12 ">Top level Domain : <span class="tolLevepDomain" style="font-weight: 200;"> Uru</span></p>
+                        <p class="col-md-6 col-sm-12 ">Top level Domain : <span class="topLevepDomain" style="font-weight: 200;"> ${selectedCountryData.tld?selectedCountryData.tld:"Not Available"} </span></p>
                     </div>
 
                     <div class="col-12 features">
                         <p class="col-md-6 col-sm-12 ">Population : <span class="population" style="font-weight: 200;">  ${selectedCountryData.population.toString()} </span></p>
 
-                        <p class="col-md-6 col-sm-12">Currencies: <span class="tolLevepDomain" style="font-weight: 200;"> Euro </span></p>
+                        <p class="col-md-6 col-sm-12">Currencies: <span class="currencies" style="font-weight: 200;"> Not Available </span></p>
                     </div>
 
 
                     <div class="col-12 features">
                         <p class="col-md-6 col-sm-12">Region: <span class="region" style="font-weight: 200;"> ${selectedCountryData.region} </span></p>
 
-                        <p class="col-md-6 col-sm-12">Languages: <span class="languages" style="font-weight: 200;"> Dutch, French, German </span>
+                        <p class="col-md-6 col-sm-12">Languages: <span class="languages" style="font-weight: 200;"> ${values} </span>
                         </p>
                     </div>
 
-                    <p class="col-12">Sub Region: <span class="subregion" style="font-weight: 200;"> Western Europe </span></p>
+                    <p class="col-12">Sub Region: <span class="subregion" style="font-weight: 200;"> ${selectedCountryData.subregion? selectedCountryData.subregion: 'Not Available'}</span></p>
 
-                    <p class="col-12">Capital: <span class="subregion" style="font-weight: 200;"> ${selectedCountryData.capital?countryData.capital:'Not available'} </span></p>
+                    <p class="col-12">Capital: <span class="subregion" style="font-weight: 200;"> ${selectedCountryData.capital?selectedCountryData.capital:'Not available'} </span></p>
 
 
                     <div class="col-12 mt-4 border-hold">
-                        <p> Border Countries: </p>
-                         <button class="borders mt-2 col-md-4" style="font-weight: 200;">France</button>
-                         <button class="borders mt-2 col-md-4" style="font-weight: 200;">Germany</button>
-                         <button class="borders mt-2 col-md-4" style="font-weight: 200;">Netherlands</button>
-                         <button class="borders mt-2 col-md-4" style="font-weight: 200;">Netherlands</button>
-                         <button class="borders mt-2 col-md-4" style="font-weight: 200;">Netherlands</button>
-                         <button class="borders mt-2 col-md-4" style="font-weight: 200;">Netherlands</button>
+                    <p> Border Countries: </p>
                     </div>
 
                 </div>
             </div>
         </div>
     `
+    contentDetails.innerHTML += countryInfo;
+
+    let borderHoldContent = "";
+    let borderHold = document.querySelector('.border-hold');
+    if (selectedCountryData.borders) {
+        let selectedBorders = selectedCountryData.borders;
+        for (let i = 0; i < selectedBorders.length; i++) {
+            borderHoldContent += `
+            <button id=${selectedBorders[i]} class="borders mt-2 col-md-4" style="font-weight: 200;"> ${selectedBorders[i]} </button>
+            `
+        }
+        borderHold.innerHTML += borderHoldContent;
+    }
+    showDetailsOfEachBorderCountry();
+}
+
+function showDetailsOfEachBorderCountry() {
+    let borders = document.querySelectorAll('.borders');
+    borders.forEach(border => {
+        border.addEventListener('click', () => {
+            localStorage.setItem('Country', JSON.stringify(border.id));
+            location.reload();
+        })
+    })
 }
--- main.js
+++ main.js
@@ -3,15 +3,21 @@ let countries; // will contain fetched data
 
 
 fetch('https://restcountries.com/v3.1/all')
-.then(res => res.json())
-.then(data => showAllCountries(data))
+.then(res => {
+    document.querySelector(".loader").style.visibility = "visible";
+    return res.json();
+})
+.then(data => {
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+    showAllCountries(data);
+})
 .catch(err => console.log(`Error: ${err}`));
 
 function showAllCountries(countriesData) {
     countries = countriesData;
     let countryInfo = "";
     let countryHold = document.querySelector('.country-hold');
-    //console.log(countries);
 
     countries.forEach(country => {
         countryInfo +=
@@ -40,20 +46,6 @@ function showAllCountries(countriesData) {
     );
     getDetailedCountryInfo();
 
-    /*
-    for (let i = 0; i < countries.length; ++i) {
-        countryInfo += ``;
-        console.log('Country:',countries[i].name.common);
-        if (countries[i].capital !== undefined) {
-            console.log("Capital:", countries[i].capital[0]);
-        } else {
-            console.log("Capital:", 'unknown');
-        }
-        console.log("Region:", countries[i].region);
-        console.log("Population:", countries[i].population.toString());
-        console.log("flag:", countries[i].flags.png);
-
-    }*/
 }
 
 function searchForCountry() {
@@ -65,29 +57,32 @@ function searchForCountry() {
         if (search.value) {
             let searchedCountry = search.value;
             const countryData = countries.find(country => country.name.common.toUpperCase() === searchedCountry.toUpperCase());
-
-                countryInfo +=
-                `<div class="card col-sm-12 col-md-6 col-lg-5 mt-4 allCountry" id="${countryData.name.common}" style="width: 18rem;">
-                <img src="${countryData.flags.png}" class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">${countryData.name.common}</h5>
-                <p class="card-text">Population:
-                <span class="population">
-                ${countryData.population.toString()}
-                </span>
-                </p>
-                <p class="card-text">Region: <span class="region">
-                ${countryData.region}
-                </span>
-                </p>
-                <p class="card-text">Capital: <span class="capital"> ${countryData.capital?countryData.capital:'Not available'}
-                </span>
-                </p>
-                </div>
-                </div>`;
+            if (countryData == undefined) {
+                return
+            }
+            countryInfo +=
+            `<div class="card col-sm-12 col-md-6 col-lg-5 mt-4 allCountry" id="${countryData.name.common}" style="width: 18rem;">
+            <img src="${countryData.flags.png}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${countryData.name.common}</h5>
+            <p class="card-text">Population:
+            <span class="population">
+            ${countryData.population.toString()}
+            </span>
+            </p>
+            <p class="card-text">Region: <span class="region">
+            ${countryData.region}
+            </span>
+            </p>
+            <p class="card-text">Capital: <span class="capital"> ${countryData.capital?countryData.capital:'Not available'}
+            </span>
+            </p>
+            </div>
+            </div>`;
+            countryHold.innerHTML = countryInfo;
+            getDetailedCountryInfo();
 
             }
-            countryHold.innerHTML = countryInfo;
     })
 }
 
@@ -97,36 +92,36 @@ searchForCountry();
 function filterByRegion() {
     let select = document.querySelector('.select');
     let countryHold = document.querySelector('.country-hold');
-    
+
     select.addEventListener('click', () => {
         let countryInfo = "";
-        //console.log(select.value);
         let selectedRegion = select.value;
-            const countryData = countries.filter(country => country.region.toUpperCase() === selectedRegion.toUpperCase());console.log(countryData);
+        const countryData = countries.filter(country => country.region.toUpperCase() === selectedRegion.toUpperCase());
 
-            countryData.forEach(country => {
-                countryInfo +=
-                `<div class="card col-sm-12 col-md-6 col-lg-5 mt-4 allCountry" id="${country.name.common}" style="width: 18rem;">
-                <img src="${country.flags.png}" class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">${country.name.common}</h5>
-                <p class="card-text">Population:
-                <span class="population">
-                ${country.population.toString()}
-                </span>
-                </p>
-                <p class="card-text">Region: <span class="region">
-                ${country.region}
-                </span>
-                </p>
-                <p class="card-text">Capital: <span class="capital"> ${country.capital?country.capital:'Not available'}
-                </span>
-                </p>
-                </div>
-                </div>`;
+        countryData.forEach(country => {
+            countryInfo +=
+            `<div class="card col-sm-12 col-md-6 col-lg-5 mt-4 allCountry" id="${country.name.common}" style="width: 18rem;">
+            <img src="${country.flags.png}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${country.name.common}</h5>
+            <p class="card-text">Population:
+            <span class="population">
+            ${country.population.toString()}
+            </span>
+            </p>
+            <p class="card-text">Region: <span class="region">
+            ${country.region}
+            </span>
+            </p>
+            <p class="card-text">Capital: <span class="capital"> ${country.capital?country.capital:'Not available'}
+            </span>
+            </p>
+            </div>
+            </div>`;
 
-                countryHold.innerHTML = countryInfo;
-            })
+            countryHold.innerHTML = countryInfo;
+        })
+        getDetailedCountryInfo();
     })
 }
 
@@ -137,8 +132,8 @@ function getDetailedCountryInfo() {
     allCountry.forEach(country => {
         country.style.cursor = 'pointer';
         country.addEventListener('click', () => {
-            console.log(country.id);
-            localStorage.setItem('Country', country.id);
+            localStorage.setItem('Country', JSON.stringify(country.id));
+            window.location.href = "./detail.html";
         })
     })
 }
